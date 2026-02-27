059e34064d97716bbc87a1660607d5ca5e8ba473
Amelia <amelia@amebit.com>
2026-02-27 19:03:53 +0100

    update mechanix-nix commit hash in images

--- images/common/mechanix.nix
+++ images/common/mechanix.nix
@@ -1,7 +1,7 @@
 rec {
 	src = builtins.fetchTarball {
-		url = "https://github.com/amelia-raine/mechanix-nix/archive/4c18094e3c70bcb9ac68df986edb1ed5872b9b62.tar.gz";
-		sha256 = "0112rpvw91g4adpi8wbj1jqyj2ngibfaxrksyd538sbi6dak6ig7";
+		url = "https://github.com/amelia-raine/mechanix-nix/archive/b97c071a7f266dea6acc449347f77368dccec84e.tar.gz";
+		sha256 = "19db4zwngvr1vrgmgdjnkzh7bji2392qvbdjx59c65nyampi95dz";
 	};
 	module = import src;
 	pkgs = import "${src}/pkgs";
